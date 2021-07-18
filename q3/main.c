7f150794f01ee0e7ed54e7d64b3483ef4054ccf2
Arthur Brito Medeiros <arthurmedeiros32@gmail.com>
2021-07-18 19:29:18 -0300

    Feature(main): finish 3rd question

--- q3/main.c
+++ q3/main.c
@@ -5,8 +5,16 @@ extern void test(lli *a, lli *b);
 
 int main(void)
 {
-    lli a = 1, b = 2;
-    test(&a, &b);
+    lli a = 0, b=0xFFFFFFFFFFFFFFFF;
+
+    printf("Before\n");
+    printf("a = %llx\n", a);
+    printf("b = %llx\n", b);
     printf("\n");
+
+    test(&a, &b);
+    printf("After\n");
+    printf("a = %llx\n", a);
+    printf("b = %llx\n", b);
     return 0;
 }
--- q4/main.c
+++ q4/main.c
@@ -6,7 +6,7 @@ void test(lli *x);
 int main(void)
 {
     // You can test with any 64 bit number and it's going to work
-    lli x = 255;
+    lli x = 0xFFFFFFFFFFFFFFFF;
     printf("Before inversion\n");
     printf("x = %016llX\n", x);
     test(&x);

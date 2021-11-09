01271408dd7f4f3a0b79e6bdad9c666ab4802aa4
Arthur Brito Medeiros <arthurmedeiros32@gmail.com>
2021-11-09 15:11:25 -0300

    add more demo code on question 2

--- q2/main.c
+++ q2/main.c
@@ -5,11 +5,10 @@ extern void test(lli *a, lli *b, lli *c, lli *x);
 
 int main(void)
 {
-    lli a = 1, b = 2, c = 26, x;
+    lli a = 0, b = 2, c = 26, x;
     printf("All true\n");
     test(&a, &b, &c, &x);
     printf("x = %lld\n\n", x);
-
 
     printf("Only A being false\n");
     a = -2;
@@ -20,5 +19,30 @@ int main(void)
     a = 1; b = 35;
     test(&a, &b, &c, &x);
     printf("x = %lld\n\n", x);
+
+    printf("Only C being false\n");
+    b = 2; c = 20;
+    test(&a, &b, &c, &x);
+    printf("x = %lld\n\n", x);
+
+    printf("Only A and B being false\n");
+    a = -1; b = 35 ;c = 26;
+    test(&a, &b, &c, &x);
+    printf("x = %lld\n\n", x);
+
+    printf("Only A and C being false\n");
+    a = -1; b = 2 ;c = 20;
+    test(&a, &b, &c, &x);
+    printf("x = %lld\n\n", x);
+
+    printf("Only B and C being false\n");
+    a = 1; b = 35;
+    test(&a, &b, &c, &x);
+    printf("x = %lld\n\n", x);
+
+    printf("A, B and C being false\n");
+    a = -1;
+    test(&a, &b, &c, &x);
+    printf("x = %lld\n", x);
     return 0;
 }

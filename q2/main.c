7a9e86071bf2e8da04d2fd6b0c50800ed2c214ff
Arthur Brito Medeiros <arthurmedeiros32@gmail.com>
2021-11-09 15:06:16 -0300

    fix assembly of 2 question

--- q2/main.c
+++ q2/main.c
@@ -5,9 +5,20 @@ extern void test(lli *a, lli *b, lli *c, lli *x);
 
 int main(void)
 {
-    lli a = 1, b = 2, c = 25, x;
+    lli a = 1, b = 2, c = 26, x;
+    printf("All true\n");
     test(&a, &b, &c, &x);
-    printf("x = %lld\n", x);
-    printf("\n");
+    printf("x = %lld\n\n", x);
+
+
+    printf("Only A being false\n");
+    a = -2;
+    test(&a, &b, &c, &x);
+    printf("x = %lld\n\n", x);
+
+    printf("Only B being false\n");
+    a = 1; b = 35;
+    test(&a, &b, &c, &x);
+    printf("x = %lld\n\n", x);
     return 0;
 }
